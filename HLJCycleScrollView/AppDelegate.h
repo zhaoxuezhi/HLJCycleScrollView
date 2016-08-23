da041d3a358e799882751422d3245d3a7dd41ff2
xiezi <xiezi@helijia.com>
2016-08-23 17:34:56 +0800

    Initial Commit

---
+++ HLJCycleScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLJCycleScrollView
+//
+//  Created by xiezi on 16/8/23.
+//  Copyright © 2016年 xiezi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLJCycleScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HLJCycleScrollView
+//
+//  Created by xiezi on 16/8/23.
+//  Copyright © 2016年 xiezi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
